29f0940f38fdddf83baf0f4db3a6f08c9afa3f0f
Shinogm <dg047359@gmail.com>
2024-04-27 23:38:35 -0700

    Refactor form animation and add framer-motion library

--- src/app/worker/page.tsx
+++ src/app/worker/page.tsx
@@ -1,14 +1,35 @@
 'use client'
+import { motion } from 'framer-motion'
 import { Back } from './components/back'
 import { WorkerForm } from './components/worker-form'
 
 export default function AdminRegister () {
   return (
-    <main className='h-dvh gap-6 md:flex md:justify-center md:items-center'>
+    <motion.main
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className='h-dvh gap-6 md:flex md:justify-center md:items-center'
+    >
       <div>
-        <h1 className='text-3xl font-bold'>Registro de Trabajadores</h1>
-        <span className='text-sm text-gray-500'>Ingreesa los datos necesarios </span>
-        <div className='
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className='text-3xl font-bold'
+        >
+          Registro de Trabajadores
+        </motion.h1>
+        <motion.span
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className='text-sm text-gray-500'
+        >
+          Ingresa los datos necesarios
+        </motion.span>
+        <div
+          className='
             items-center
             justify-center
             flex
@@ -19,10 +40,14 @@ export default function AdminRegister () {
           <Back url='/worker/all-workers'>Seccion de trabajadores</Back>
         </div>
       </div>
-      <section className='md:w-1/2'>
+      <motion.section
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        className='md:w-1/2'
+      >
         <WorkerForm />
-      </section>
-
-    </main>
+      </motion.section>
+    </motion.main>
   )
 }
